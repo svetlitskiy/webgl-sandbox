b37380b8195da0989d6ccc26590b28a59ef19b1b
Aleksey Svetlitskiy <svetlitskiy.a@n-t.io>
2020-10-26 17:25:42 +0300

    lesson 4 (intermediate commit)

--- src/main.ts
+++ src/main.ts
@@ -21,19 +21,35 @@ if (!gl) {
 // draw
 
 const vertexData = [
-    0, 1, 0,
-    1, -1, 0,
-    -1, -1, 0,
+    0, 0.9, 0,          // V1.position
+    0.3, -0.4, -0.5,    // V2.position
+    -1, -1, 0,          // V3.position
 ];
 
-const buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+const colorData: number[] = [
+    1, 0, 0,    // V1.color
+    0, 1, 0,    // V2.color
+    0, 0, 1     // V3.color
+];
+
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.DYNAMIC_DRAW);
+
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.DYNAMIC_DRAW);
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
+precision mediump float;
+
 attribute vec3 position;
+attribute vec3 color;
+varying vec3 vColor;
+
 void main() {
+    vColor = color;
     gl_Position = vec4(position, 1);
 }
 `);
@@ -41,8 +57,10 @@ gl.compileShader(vertexShader);
 
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, `
+varying vec3 vColor;
+
 void main() {
-    gl_FragColor = vec4(1, 0, 0, 1);
+    gl_FragColor = vec4(vColor, 0, 0, 1);
 }`);
 gl.compileShader(fragmentShader);
 
@@ -53,7 +71,13 @@ gl.linkProgram(program);
 
 const positionLocation = gl.getAttribLocation(program, `position`);
 gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0,0);
+
+const colorLocation = gl.getAttribLocation(program, `color`);
+gl.enableVertexAttribArray(colorLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0,0);
 
 gl.useProgram(program);
 gl.drawArrays(gl.TRIANGLES, 0, 3);
